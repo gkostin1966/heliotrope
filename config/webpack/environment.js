cae38cc24593f8254e9834a77031c544f350c793
Greg Kostin <gkostin@umich.edu>
2020-08-10 13:04:25 -0400

    yarn add jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+// To avoid having to import $ from 'jquery' in every file that uses jQuery, make the following change to config/webpack/environment.js:
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery'
+}))
+
 module.exports = environment
