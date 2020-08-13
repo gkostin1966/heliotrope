7d0653072f21c8050208ac79ece4041e30d0e0e4
Greg Kostin <gkostin@umich.edu>
2020-08-13 16:04:54 -0400

    yarn lint spec/javascript

--- spec/javascript/packs/hello_react.spec.js
+++ spec/javascript/packs/hello_react.spec.js
@@ -3,20 +3,20 @@
 // and it prevents rendering the entire DOM tree. However,
 // you might need other rendering types, in this case, check the Enzyme’s documentation.
 
-import React from 'react'
-import { shallow } from 'enzyme'
-import HelloReact from 'packs/hello_react'
+import React from 'react';
+import { shallow } from 'enzyme';
+import HelloReact from 'packs/hello_react';
 
 describe('HelloReact component', () => {
   describe('when a name is given as a prop', () => {
     it('render Hello React!', () => {
-      expect(shallow(<HelloReact name="React" />).text()).toBe('Hello React!')
-    })
-  })
+      expect(shallow(<HelloReact name="React" />).text()).toBe('Hello React!');
+    });
+  });
 
   describe('when no name is given', () => {
     it('render Hello David!', () => {
-      expect(shallow(<HelloReact />).text()).toBe('Hello David!')
-    })
-  })
-})
+      expect(shallow(<HelloReact />).text()).toBe('Hello David!');
+    });
+  });
+});
